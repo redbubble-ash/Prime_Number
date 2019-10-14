90c0cd58881451fde5a885de864666eebf4b6cc3
Jing Xie <xiejing418@gmail.com>
2019-10-14 13:10:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate_Prime(100);
+       }
+
+        static void Calculate_Prime(int n)
+        {
+            int i;
+            bool ifPrime = false;
+
+
+            for (i=2 ; i<=n; i++)
+            {
+                //Console.WriteLine(i);
+
+                for (int j = 2; j<i; j++)
+                {
+                    if (i%j == 0)
+                    {
+                        ifPrime = false;
+                        break;
+                    }
+                    else
+                    {
+                        ifPrime = true;
+                    }
+                }
+
+                if(ifPrime == true)
+                {
+                    
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+
+    }
+}
